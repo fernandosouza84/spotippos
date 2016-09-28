0932096893b4ee6ca46223b9035e510f0d5a48bd
Fernando Souza <fernando.de.souza@gmail.com>
2016-09-27 22:31:53 -0300

    first commit

---
+++ src/main/java/com/spotipos/SpotiposApplication.java
@@ -0,0 +1,12 @@
+package com.spotipos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpotiposApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpotiposApplication.class, args);
+	}
+}
